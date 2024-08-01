aa91381a7b8aec0a1183ef9b2cba699eb021b8af
MoamenZakariaMohamed <92689083+MoamenZakariaMohamed@users.noreply.github.com>
2024-08-01 15:39:36 +0300

    implement swagger

---
+++ src/main/java/com/itida/session/management/infrastructure/security/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.itida.session.management.infrastructure.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/**
+ * The type Open api config.
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Tracker"),
+                description = "Tracker Project",
+                title = "Tracker - Swagger API Documentation",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Server",
+                        url = "/"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
+
+
